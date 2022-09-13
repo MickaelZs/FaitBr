5b2b2a2d55cb3c5485c7cdb211ea256c11bd7c94
C4SHZIN <kircaua5661@gmail.com>
2022-09-13 18:11:50 -0300

    Rec quase feito

--- site/src/pages/recuperacao/index.jsx
+++ site/src/pages/recuperacao/index.jsx
@@ -3,17 +3,24 @@ import './index.scss'
 export default function Rec(){
     return(
         <main className='pagina-rec'>
+            <div className='sub-mae'>
+                
             <section className='faixa-rec1'>
-            <h1 className='h1'>Recuperar senha</h1>
-            <h3 className='h3'>Um código de confirmação foi enviado para seu e-mail </h3>
-            <p>kir***@gmail.com</p>
+            
+            <h1 className='h1'>Recuperar Senha</h1>
+            <div className='bah'>
+            <h3 className='h3'>Um codigo de confirmação foi enviado para o email: </h3>
+            
+            <p className='p'>kir***@gmail.com</p>
+            </div>
+
             </section>
             <div className='faixa-input'>
                 <input type="number"></input>
                 <input type="number"></input>
                 <input type="number"></input>
                 <input type="number"></input>
-
+            </div>
             </div>
         </main>
     );
