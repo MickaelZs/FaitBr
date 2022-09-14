8921e673882bd36916d75ef9e4d04d142a455f37
C4SHZIN <kircaua5661@gmai>
2022-09-14 18:16:12 -0300

    pagina cadastrar musica

---
+++ site/src/pages/cadastrarmsc/index.jsx
@@ -0,0 +1,33 @@
+import CardCadastromsc from '../../components/comp-cadastrar-musica'
+import './index.scss'
+import Emojihome from './casinha.svg'
+import cadastrarmusica from './cadastrarmusica.svg'
+import consulta from './consulta.svg'
+import msccadastrada from './msccadastrada.svg'
+import artistacadastrado from './artistacadastrado.svg'
+import sair from './sair.svg'
+import artista from './artista.svg'
+import fitbr from './fitbr.svg'
+
+export default function Cadastromsc(){
+    return(
+        <main className='pagina-cadastro-musica'>
+            <section className='faixa-menu'>
+                <img src={fitbr} width='150px'/>
+                <CardCadastromsc categoria={Emojihome} nome="Home"/>
+                <CardCadastromsc categoria={artista} nome="Cadastrar Artista"/>
+                <CardCadastromsc categoria={cadastrarmusica} nome="Cadastrar Música"/>
+                <CardCadastromsc categoria={consulta} nome="Consultar"/>
+                <CardCadastromsc categoria={msccadastrada} nome="Músicas Cadastradas"/>
+                <CardCadastromsc categoria={artistacadastrado} nome="Artistas cadastrados"/>
+                <CardCadastromsc categoria={sair} nome="Sair"/>
+            </section>
+            <section className='faixa-cadastrar'>
+                <div className='faixa-branca'>
+
+                </div>
+
+            </section>
+        </main>
+    )
+}
