cb05ecb85b86136de45626bffa72ea88a7c97819
C4SHZIN <kircaua5661@gmail.com>
2022-09-18 17:09:43 -0300

    cadastrarmsc feito

--- site/src/pages/cadastrarmsc/index.jsx
+++ site/src/pages/cadastrarmsc/index.jsx
@@ -1,46 +1,60 @@
 import CardCadastromsc from '../../components/comp-cadastrar-musica'
 import './index.scss'
-import Emojihome from './casinha.svg'
-import cadastrarmusica from './cadastrarmusica.svg'
-import consulta from './consulta.svg'
-import msccadastrada from './msccadastrada.svg'
-import artistacadastrado from './artistacadastrado.svg'
-import sair from './sair.svg'
-import artista from './artista.svg'
-import fitbr from './fitbr.svg'
-import imagem from './imagem.svg'
-import Cardinput from '../../components/inputs'
 
 
 export default function Cadastromsc(){
     return(
         <main className='pagina-cadastro-musica'>
-            <section className='faixa-menu'>
-                <img src={fitbr} width='150px'/>
-                <CardCadastromsc categoria={Emojihome} nome="Home"/>
-                <CardCadastromsc categoria={artista} nome="Cadastrar Artista"/>
-                <CardCadastromsc categoria={cadastrarmusica} nome="Cadastrar Música"/>
-                <CardCadastromsc categoria={consulta} nome="Consultar"/>
-                <CardCadastromsc categoria={msccadastrada} nome="Músicas Cadastradas"/>
-                <CardCadastromsc categoria={artistacadastrado} nome="Artistas cadastrados"/>
-                <CardCadastromsc categoria={sair} nome="Sair"/>
-            </section>
-            <section className='faixa-cadastrar'>
-                <div className='faixa-branca'>
-                    <div>
-                        <p>Adicionar música</p>
-                        <div>
-                        <img src={imagem}/>
-                        </div>
-                    </div>
-                    <div>
-                        <Cardinput/>
-                        <Cardinput/>
-                            
+        <section className='faixa-icons'>
+            <img src="images/fitbr.svg"  width='150px' className='image-fitbr'/>
+            <div className='icons'>
+            <CardCadastromsc categoria='images/casinha.svg' nome='Home' cardscss='div-card1' pclas='p'/>
+           
+            <CardCadastromsc categoria='images/artista.svg' nome='Cadastrar Artistas' cardscss='div-card1' pclas='p'/>
+           
+             <div className='background'>
+            <CardCadastromsc categoria='images/cadastrarmusica.svg' nome='Cadastrar Musica' cardscss='div-card1' pclas='p'/>
+            </div>
+            <CardCadastromsc categoria='images/consulta.svg' nome='Consultar' cardscss='div-card1' pclas='p'/>
+            <CardCadastromsc categoria='images/msccadastrada.svg' nome='Musicas Cadastradas' cardscss='div-card1' pclas='p'/>
+            <CardCadastromsc categoria='images/artistacadastrado.svg' nome='Artistas Cadastrados' cardscss='div-card1' pclas='p'/>                
+            </div>
+            <div className='div-sair'>
+            <CardCadastromsc categoria='images/sair.svg' nome='Sair' cardscss='div-card2' pclas='p2'/> 
+            </div>
+        </section>
+        <section className='faixa-cadastro'>
+            <div className='margin-cadastro'>
+                <div className='div1-cadastro'>
+                    <p className='p'>
+                        Adicionar capa
+                    </p>
+                    <div className='border-image'>
+                        <img src='./images/image-bottom212.svg' width='170px'/>
                     </div>
                 </div>
+                <div className='div2-cadastro'>
+                    <div className='div-input'>
+                    <input type="text"  placeholder="Artista"  required></input>
+                    </div>
+                    <div className='div-input'>
+                    <input type="text" placeholder="Nome" required></input>
+                    </div>
+                    <div className='div-input'>
+                    <input type="text" placeholder="Musica" required></input>
+                    </div>
+                    <div className='div-input'>
+                    <input type="text" placeholder="Gênero" required></input>
+                    </div>
+                    <div className='div-input'>
+                    <input type="number" placeholder="Tempo" required></input>
+                    </div>
+                    
+                            <button className='botao'>Cadastrar</button>
+                </div>
+            </div>
 
-            </section>
-        </main>
+        </section>
+    </main>
     )
 }
