43be47967b050cc4b87c5f47f5a4be6dd3fc1d85
MickaelZs <mickaelcosta431@gmail.com>
2022-09-20 18:17:59 -0300

    em produção

--- site/src/pages/artista cadastrado/index.js
+++ site/src/pages/artista cadastrado/index.js
@@ -3,8 +3,18 @@ import './index.scss'
 
 export default function Index () {
     return(
-        <div>
+        <div className="pag-consulta">
+            
             <Menu />
+            <div className="consulta">
+                <div className="barra-pesquisa">
+                    <input  type="text" placeholder="Buscar" />
+                    <img src="images/icon-pesquisa.png" />
+                </div>
+                <div>
+                    th
+                </div>
+            </div>
         </div>
     )
 }
