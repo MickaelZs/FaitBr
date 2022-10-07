7525b7fdba3fbd77026d281a84831f5f9ed95183
MickaelZs <mickaelcosta431@gmail.com>
2022-10-07 00:49:03 -0300

    db

--- db/dll.sql
+++ db/dll.sql
@@ -1,5 +1,5 @@
 create database fitbr;
-
+drop database fitbr;
 
 use fitbr;
 
@@ -15,23 +15,12 @@ id_usuario int primary key auto_increment,
 nm_nome varchar(200),
 dt_nasc datetime,
 ds_email varchar(200),
-ds_senha varchar(200)
+ds_senha varchar(200),
+img_imagem varchar(200)
 );
 
 insert into tb_usuario (nm_nome,dt_nasc,ds_email, ds_senha)
-values ('usu','2005-05-05', 'usuario', 123 );
-
-
-
-
-create table tb_loginUsuario(
-id_loginusuario int primary key auto_increment,
-ds_email varchar(200),
-ds_senha varchar(200)
-);
-
-insert into tb_loginUsuario (ds_email, ds_senha)
-values ( 'usuario', 123 );
+values ('usu','2005-05-05', 'BRUNO', 123 );
 
 
 create table tb_loginadm (
@@ -43,23 +32,11 @@ ds_senha varchar (200)
 insert into tb_loginadm (ds_email, ds_senha)
 values ( 'adm', 123 );
 
-create table tb_artistas(
-id_artistas int primary key auto_increment,
-id_genero int,
-nm_artistas varchar(200),
-ds_genero varchar(200),
-ds_sobre varchar(200),
-img_artista varchar(200),
-foreign key (id_genero) references tb_genero (id_genero)
-);
-
-
 create table tb_genero(
 id_genero int primary key auto_increment,
 nm_genero varchar (100),
 img_genero varchar(200)
 );
-
 
 insert into tb_genero (nm_genero)
 values ( 'Funk');
@@ -78,6 +55,23 @@ values ( 'Brega funk');
 insert into tb_genero (nm_genero)
 values ( 'Rock');
 
+
+create table tb_artistas(
+id_artistas int primary key auto_increment,
+id_genero int,
+nm_artistas varchar(200),
+ds_genero varchar(200),
+ds_sobre varchar(200),
+img_artista varchar(200),
+foreign key (id_genero) references tb_genero (id_genero)
+);
+
+select tb_artistas.id_artistas,
+nm_artistas,
+ds_sobre,
+nm_genero
+from tb_artistas
+inner join tb_genero on tb_artistas.id_genero = tb_genero.id_genero;
 
 create table tb_musicas(
 id_musica int primary key auto_increment,
